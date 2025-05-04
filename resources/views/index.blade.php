3359820adff3fce56da489580ed4d0c05a0ccb91
kei <dr.kei21@gmail.com>
2025-05-04 17:49:43 +0900

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+    <title>laravel+TypeScript</title>
+    @viteReactRefresh
+    @vite([
+    'resources/css/app.css',
+    'resources/ts/index.tsx',
+    ])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
+
